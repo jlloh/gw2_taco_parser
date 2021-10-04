4db3cbdd310886566fd2f2281baf0d4a2c131419
Jia Long Loh <jlloh89@gmail.com>
2021-10-04 22:03:14 +0800

    Working xml parser

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use crate::parser::parse_trail;
+use crate::parser::{parse_trail, process_taco_data};
+use std::fs::File;
+use std::io::prelude::*;
 
 pub mod parser;
 
 fn main() {
     let aaa = include_bytes!("trail_bloodstone_fen_1.trl");
     let result = parse_trail(aaa).unwrap();
-    println!("{:?}", result.1);
+    // println!("{:?}", result.1);
+    let contents = include_str!("dw_coral.xml");
+    let xml_parsed = parser::parse_xml(&contents);
+    println!("{:#?}", xml_parsed);
+    println!("{:#?}", process_taco_data(xml_parsed));
 }
+ 
--- src/parser.rs
+++ src/parser.rs
@@ -4,7 +4,9 @@ use nom::{
     IResult,
     sequence::tuple};
 use nom::multi::fold_many0;
-
+use serde::Deserialize;
+use serde_xml_rs;
+use std::collections::HashMap;
 
 #[derive(Debug,PartialEq)]
 pub struct Trail {
@@ -12,7 +14,6 @@ pub struct Trail {
     map_id: i32,
     coordinates: Vec<TrailCoordinates>
 }
-
 
 #[derive(Debug,PartialEq)]
 pub struct TrailCoordinates {
@@ -44,3 +45,139 @@ pub fn parse_trail(input: &[u8]) -> IResult<&[u8], Trail> {
         coordinates: vec
     }))
 }
+
+#[derive(Debug, Deserialize)]
+pub struct OverlayData {
+    #[serde(rename = "MarkerCategory")]
+    marker_category: MarkerCategory,
+    #[serde(rename = "POIs")]
+    pois: POIArrayContainer,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct MarkerCategoryArrayContainer {
+    name: String,
+    #[serde(rename = "$value")]
+    marker_category_array: Vec<MarkerCategory>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct MarkerCategory {
+    name: String,
+    #[serde(rename = "iconFile")]
+    icon_file: Option<String>,
+    #[serde(rename = "MarkerCategory")]
+    marker_category: Option<MarkerCategoryArrayContainer>,
+    #[serde(rename = "heightOffset")]
+    height_offset: Option<f32>
+}
+
+#[derive(Debug, Deserialize)]
+pub struct POIArrayContainer {
+    #[serde(rename = "$value")]
+    poi_array: Vec<PoiItems>,
+}
+
+#[derive(Debug, Deserialize)]
+struct POI {
+    #[serde(rename = "MapID")]
+    map_id: u32,
+    xpos: f32,
+    ypos: f32,
+    zpos: f32,
+    #[serde(rename = "iconFile")]
+    icon_file: Option<String>,
+    #[serde(rename = "heightOffset")]
+    height_offset: Option<f32>
+}
+
+#[derive(Debug, Deserialize)]
+struct TrailMetadata {
+    #[serde(rename = "trailData")]
+    trail_data: String,
+    texture: String,
+    color: String,
+    #[serde(rename = "iconFile")]
+    icon_file: Option<String>
+}
+
+#[derive(Debug, Deserialize)]
+enum PoiItems {
+    POI(POI),
+    #[serde(rename = "Trail")]
+    TrailMetadata(TrailMetadata)
+}
+
+//https://docs.rs/serde-xml-rs/0.5.1/serde_xml_rs/
+// TODO: Accept Trail by making it an enum like the example
+pub fn parse_xml(xml: &str ) -> OverlayData {
+    //let root: minidom::Element = xml.parse().unwrap();
+    //println!("{:#?}", root);
+    // println!("{:?}", xml);
+    let data: OverlayData = serde_xml_rs::from_str(xml).unwrap();
+    data
+}
+
+pub fn process_taco_data(input: OverlayData) -> HashMap<u32, Vec<[f32; 3]>> {
+    let root_marker_category = input.marker_category;
+
+    let marker_category_array_container = root_marker_category.marker_category.unwrap();
+
+    let marker_category_array = marker_category_array_container.marker_category_array;
+
+    let poi_array = input.pois.poi_array;
+
+    let folder_function = | mut acc: HashMap<u32, Vec<[f32; 3]>>, item: &PoiItems | {
+        match item {
+            PoiItems::POI(poi)=> { 
+                let map_id = poi.map_id;
+                let array_ = acc.get(&map_id);
+                match array_ {
+                    Some(vec_) => {
+                        let mut res = vec_.clone();
+                        // res.push(xpos);
+                        res.push([poi.xpos, poi.ypos, poi.zpos]);
+                        acc.insert(map_id, res);
+                    }
+                    _ => {
+                        let mut vec_ = Vec::new();
+                        vec_.push([poi.xpos, poi.ypos, poi.zpos]);
+                        acc.insert(map_id, vec_);
+                    }
+                }
+            }
+            PoiItems::TrailMetadata(trail_metadata) => {
+                let z = &trail_metadata.trail_data;
+            }
+        }
+        return acc;
+    };
+
+    let map_name: HashMap<_, _> = HashMap::new();
+    let result = poi_array.iter().fold(map_name, folder_function);
+    return result
+}
+
+struct Converted {
+    // icons: Vec<String>,
+    paths: Vec<Path>
+}
+
+struct Path {
+    texture: String,
+    points: Vec<[f32; 3]>
+}
+
+// Convert to map{ map_id: 
+    // {
+    //     "1330": {
+    //         "icons": [
+    //             {
+    //                 "position": [
+    //                     -246.114,
+    //                     19.9021,
+    //                     -586.05
+    //                 ],
+    //                 "texture": "data/coral.png"
+    //             },]
+    //          "paths": [{"texture": "", points: [[1, 2, 3], ...]}]
