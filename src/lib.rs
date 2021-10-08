43e0be827d988ce659035dd19a0ed98bbd76902d
Jia Long Loh <jlloh89@gmail.com>
2021-10-08 12:07:56 +0800

    Expose only one function for simplicity, that does json deserialising

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,8 @@
 mod trail_parser;
-pub mod xml_parser;
+mod xml_parser;
+
+pub fn taco_to_json_str(folder_name: String, xml_file: String) -> String {
+    let parsed = xml_parser::process_taco_data(folder_name, xml_file);
+    let json_string = serde_json::to_string(&parsed).unwrap();
+    return json_string
+}
